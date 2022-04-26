c1097e7f8d462f491b9ad9a78d9d02cbb3e165bc
ajit Goel <ajitgoel@gmail.com>
2022-04-26 15:03:10 -0500

    4/26/2022

--- Jetbrains/Jetbrains-licence-3.bat
+++ Jetbrains/Jetbrains-licence-3.bat
@@ -3,7 +3,19 @@ cd %APPDATA%\JetBrains
 del "PermanentDeviceId"
 del "PermanentUserId"
 rmdir "consentOptions" /s /q
-cd %APPDATA%\JetBrains\PHPStorm2021.1*
+cd %APPDATA%\JetBrains\IntelliJIdea2021.1*
 rmdir "eval" /s /q
 del "options\other.xml"
 reg delete "HKEY_CURRENT_USER\Software\JavaSoft\Prefs\jetbrains"
+
+:: Delete eval folder with licence key and options.xml which contains a reference  to it
+for %%I in ("WebStorm", "IntelliJ", "CLion", "Rider", "GoLand", "PhpStorm") do (
+  for /d %%a in ("%USERPROFILE%\.%%I*") do (
+    rd /s /q "%%a/config/eval"
+    del /q "%%a\config\options\other.xml"
+  )
+)
+
+:: Delete registry key and jetbrains folder (not sure if needet but however)
+rmdir /s /q "%APPDATA%\JetBrains"
+reg delete "HKEY_CURRENT_USER\Software\JavaSoft" /f
